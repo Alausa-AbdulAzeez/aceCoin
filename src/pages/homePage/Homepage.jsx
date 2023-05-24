2668d2fccd06f02f6ee33393b5fcb5213e5c84b7
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-25 00:16:13 +0100

    created order details component

--- src/pages/homePage/Homepage.jsx
+++ src/pages/homePage/Homepage.jsx
@@ -3,19 +3,15 @@ import "./homepage.scss";
 import verifiedCheck from "../../assets/verified-badge.svg";
 import mastercard from "../../assets/mc_symbol.svg";
 import dots from "../../assets/dots.svg";
+import closeIcon from "../../assets/close.svg";
+import { RiPencilFill } from "react-icons/ri";
+import OrderDetails from "../../components/orderDetails/OrderDetails";
 
 const Homepage = () => {
   return (
     <div className="wrapper">
       <div className="close-btn">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="ionicon"
-          viewBox="0 0 512 512"
-        >
-          <title>Close</title>
-          <path d="M289.94 256l95-95A24 24 0 00351 127l-95 95-95-95a24 24 0 00-34 34l95 95-95 95a24 24 0 1034 34l95-95 95 95a24 24 0 0034-34z" />
-        </svg>
+        <img src={closeIcon} alt="close" />
       </div>
 
       <div className="left">
@@ -46,34 +42,8 @@ const Homepage = () => {
                     </p>
                   </div>
                   <button className="edit">
-                    <svg
-                      version="1.1"
-                      id="Layer_1"
-                      xmlns="http://www.w3.org/2000/svg"
-                      // xmlns:xlink='http://www.w3.org/1999/xlink'
-                      x="0px"
-                      y="0px"
-                      viewBox="0 0 512 512"
-                      // xml:space='preserve'
-                    >
-                      <g>
-                        <g>
-                          <path
-                            d="M311.18,78.008L32.23,356.958L0.613,485.716c-1.771,7.209,0.355,14.818,5.604,20.067
-                                                c5.266,5.266,12.88,7.368,20.067,5.604l128.759-31.617l278.95-278.95L311.18,78.008z M40.877,471.123l10.871-44.271l33.4,33.4
-                                                L40.877,471.123z"
-                          />
-                        </g>
-                      </g>
-                      <g>
-                        <g>
-                          <path
-                            d="M502.598,86.818L425.182,9.402c-12.536-12.536-32.86-12.536-45.396,0l-30.825,30.825l122.812,122.812l30.825-30.825
-                                                C515.134,119.679,515.134,99.354,502.598,86.818z"
-                          />
-                        </g>
-                      </g>
-                    </svg>
+                    <RiPencilFill className="pen" />
+
                     <span className="btn-desc">Edit</span>
                   </button>
                 </div>
@@ -138,85 +108,13 @@ const Homepage = () => {
                   <img src={dots} alt="dots" className="trailing-icon" />
                 </div>
               </div>
-              <button className="btn-submit">pay now</button>
+              <div className="btn-submit">pay now</div>
             </form>
           </section>
         </div>
       </div>
       <div className="right">
-        <div className="checkout">
-          <span className="blu"></span>
-          <div className="credit-card">
-            <div className="credit-card__head">
-              <img src="assets/chip.png" alt="" className="chip" />
-              <img src="assets/wifi.png" alt="" className="wifi" />
-            </div>
-            <div className="credit-card__body">
-              <span className="holder">jonathan micheal</span>
-              <div className="card-digits">
-                <div className="hidden-digits">
-                  <span className="circle"></span>
-                  <span className="circle"></span>
-                  <span className="circle"></span>
-                  <span className="circle"></span>
-                </div>
-                <span className="visible-digits">3456</span>
-              </div>
-            </div>
-            <div className="credit-card__footer">
-              <span className="expiry">09/22</span>
-              <img src="assets/mastercard-2.svg" alt="" className="logo" />
-            </div>
-            <div className="card__design">
-              <span className="inner"></span>
-            </div>
-          </div>
-          <div className="checkout-details">
-            <div className="purchase-data">
-              <div className="data">
-                <span className="title">company</span>
-                <span className="value">
-                  <img src="assets/apple-13.svg" alt="" />
-                  <span className="inner-text">apple</span>
-                </span>
-              </div>
-              <div className="data">
-                <span className="title">order number</span>
-                <span className="value">
-                  <span className="inner-text">1266201</span>
-                </span>
-              </div>
-              <div className="data">
-                <span className="title">product</span>
-                <span className="value">
-                  <span className="inner-text">macbook air</span>
-                </span>
-              </div>
-              <div className="data">
-                <span className="title">VAT (20%)</span>
-                <span className="value">
-                  <span className="inner-text">$100.00</span>
-                </span>
-              </div>
-            </div>
-            <div className="divider">
-              <div className="left"></div>
-              <div className="right"></div>
-            </div>
-            <div className="purchase-total">
-              <div className="total">
-                <span className="desc">You have to Pay</span>
-                <div className="t-amount">
-                  <h1 className="amount">
-                    549<sub>.99</sub>
-                  </h1>
-                  <span className="symbol">USD</span>
-                </div>
-              </div>
-              <img src="assets/docket.png" alt="" />
-            </div>
-          </div>
-        </div>
+        <OrderDetails />
       </div>
     </div>
   );
